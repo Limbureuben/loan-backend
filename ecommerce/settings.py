98a573e1db78a2ecefaad47c8ea30a72f782ee4b
Limbureuben <limbureuben4@gmail.com>
2025-08-27 12:52:41 +0300

    changes

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -83,24 +83,37 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'Mkopo',
+#         'NAME': 'mkopo',
 #         'USER': 'postgres',             # Use existing PostgreSQL user
 #         'PASSWORD': '12345', # Replace with actual password
 #         'HOST': 'localhost',
 #         'PORT': '5432',
 #     }
 # }
+
+
+
+
+import dj_database_url
+import os
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
+}
+
 
 AUTH_USER_MODEL = 'myapp.CustomUser'
 
